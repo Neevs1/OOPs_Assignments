33ac84e3deedae4be58cafcbd84c524e1bb974bf
Neevs1 <neevan.redkar@gmail.com>
2024-08-02 14:55:34 +0530

    Added addition function

--- Assignment1/src/display.java
+++ Assignment1/src/display.java
@@ -3,24 +3,25 @@ import java.util.*;
 class complex{
 	Scanner sc = new Scanner(System.in);
 	String input1,input2,type;
+	int real,imaginary;
 	complex(){
 		System.out.println("Enter first number");
 		input1 = sc.nextLine();
 		if(input1.charAt((input1.length())-1)=='i'&&input1.contains("+")) {
 			System.out.println("Entered number is complex number");
-			int real = Integer.parseInt(input1.substring(0, input1.indexOf("+")));
-			int imaginary = Integer.parseInt(input1.substring(input1.indexOf("+")+1,input1.length()-1));
+			 real = Integer.parseInt(input1.substring(0, input1.indexOf("+")));
+			 imaginary = Integer.parseInt(input1.substring(input1.indexOf("+")+1,input1.length()-1));
 			System.out.println("Real part is "+real+" and imaginary part is "+imaginary);
 			type = "Imaginary";
 		}else if(input1.charAt((input1.length())-1)=='i'&&(input1.contains("+")==false)){
 			System.out.println("Entered number is imaginary number");
-			int real = 0;
-			int imaginary = Integer.parseInt(input1.substring(0,input1.length()-1));
+			real = 0;
+			imaginary = Integer.parseInt(input1.substring(0,input1.length()-1));
 			System.out.println("Real part is "+real+" and imaginary part is "+imaginary);
 		}else {
 			System.out.println("Entered number is real");
-			int real = Integer.parseInt(input1);
-			int imaginary = 0;
+			 real = Integer.parseInt(input1);
+			 imaginary = 0;
 			System.out.println("Real part is "+real+" and imaginary part is "+imaginary);
 		}
 	}
@@ -28,22 +29,27 @@ class complex{
 		
 		if(input2.charAt((input2.length())-1)=='i'&&input2.contains("+")) {
 			System.out.println("Entered number is complex number");
-			int real = Integer.parseInt(input2.substring(0, input2.indexOf("+")));
-			int imaginary = Integer.parseInt(input2.substring(input2.indexOf("+")+1,input2.length()-1));
+			 real = Integer.parseInt(input2.substring(0, input2.indexOf("+")));
+			 imaginary = Integer.parseInt(input2.substring(input2.indexOf("+")+1,input2.length()-1));
 			System.out.println("Real part is "+real+" and imaginary part is "+imaginary);
 		}else if(input2.charAt((input2.length())-1)=='i'&&(input2.contains("+")==false)){
 			System.out.println("Entered number is imaginary number");
-			int real = 0;
-			int imaginary = Integer.parseInt(input2.substring(0,input2.length()-1));
+			 real = 0;
+			 imaginary = Integer.parseInt(input2.substring(0,input2.length()-1));
 			System.out.println("Real part is "+real+" and imaginary part is "+imaginary);
 			
 		}else {
 			System.out.println("Entered number is real");
-			int real = Integer.parseInt(input2);
-			int imaginary = 0;
+			 real = Integer.parseInt(input2);
+			 imaginary = 0;
 			System.out.println("Real part is "+real+" and imaginary part is "+imaginary);
 		}
 		
+	}
+	void addition(complex b) {
+		real = real+b.real;
+		imaginary = imaginary+b.imaginary;
+		System.out.println("Sum is "+real+"+"+imaginary+"i");
 	}
 	
 }
@@ -55,6 +61,14 @@ public class display {
 		System.out.println("Enter second number");
 		String input2 = sc.nextLine();
 		complex c2 = new complex(input2);
+		System.out.println("Enter operation of choice");
+		int choice = sc.nextInt();
+		switch(choice) {
+		case 1:
+			c.addition(c2);
+		default:
+			System.out.println("Invalid operation");
+		}
 		
 	}
 
