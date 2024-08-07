b9d6f4c1c1d935e1e6cf69a8e479652235c92455
ShubhamRS13 <pavanpatil3546@gmail.com>
2024-08-07 14:20:18 +0530

    Adding comments

--- Assignment1/src/display.java
+++ Assignment1/src/display.java
@@ -2,7 +2,7 @@
  * Name:- Neevan Abhijit Redkar
  * Roll no:- 3069
  * CRN no:- 23013069
- * Division :- Second YEar(S.E.)
+ * Division :- Second Year(S.E.)
  * Batch :- S3
  */
 
@@ -13,46 +13,50 @@ class complex{
 	Scanner sc = new Scanner(System.in);
 	String input1,input2,type;
 	int real,imaginary;
-	complex(){
+	complex(){ //default constructor 
 		System.out.println("Enter first number (Enter 0-(number)i if negative imaginary)");
-		input1 = sc.nextLine();
-		if(input1.charAt((input1.length())-1)=='i'&&(input1.contains("+")||(input1.contains("-")))) {
+		input1 = sc.nextLine();//takes string input
+		if(input1.charAt((input1.length())-1)=='i'&&(input1.contains("+")||(input1.contains("-")))) { //checks whether input is complex
 			System.out.println("Entered number is complex number");
-			if(input1.contains("+")) {
-				if((input1.charAt(0)!='-')) {
-			 real = Integer.parseInt(input1.substring(0, input1.indexOf("+")));
-			 imaginary = Integer.parseInt(input1.substring(input1.indexOf("+")+1,input1.length()-1));
-				}else {
-					real = Integer.parseInt(input1.substring(1, input1.indexOf("+")))*-1;
-					imaginary = Integer.parseInt(input1.substring(input1.indexOf("+")+1,input1.length()-1));
+			if(input1.contains("+")) { //checks if complex number has a positive imaginary part
+				if((input1.charAt(0)!='-')) {//checks whether entered input has negative real part
+			 real = Integer.parseInt(input1.substring(0, input1.indexOf("+")));//parses substring before '+' sign as real part
+			 imaginary = Integer.parseInt(input1.substring(input1.indexOf("+")+1,input1.length()-1)); // parses substring after '+' as imaginary part
+				}else { //if input is negative
+					real = Integer.parseInt(input1.substring(1, input1.indexOf("+")))*-1;//parses before '+' substring as real part, multiplies by -1
+					imaginary = Integer.parseInt(input1.substring(input1.indexOf("+")+1,input1.length()-1));//parses substring after '+' as imaginary part
 				}
-			}else {
-				if((input1.charAt(0)!='-')) {
-					 real = Integer.parseInt(input1.substring(0, input1.indexOf("-")));
-					 imaginary = Integer.parseInt(input1.substring(input1.indexOf("-")+1,input1.length()-1))*-1;
+			}else {//if entered number has negative imaginary part
+				if((input1.charAt(0)!='-')) { //if number has  real part 
+					 real = Integer.parseInt(input1.substring(0, input1.indexOf("-")));//parses substring till '-' sign as real
+					 imaginary = Integer.parseInt(input1.substring(input1.indexOf("-")+1,input1.length()-1))*-1;//parses substring after '-' as imaginary
 				}else {
-					real = Integer.parseInt(input1.substring(1, input1.indexOf("-",input1.indexOf("-")+1)))*-1;
-					imaginary = Integer.parseInt(input1.substring(input1.indexOf("-",input1.indexOf("-")+1)+1,input1.length()-1))*-1;
+					real = Integer.parseInt(input1.substring(1, input1.indexOf("-",input1.indexOf("-")+1)))*-1;//parses substring till '-' sign as real, multiply by -1
+					imaginary = Integer.parseInt(input1.substring(input1.indexOf("-",input1.indexOf("-")+1)+1,input1.length()-1))*-1;//parses substring after '-' as imaginary
 				}
 						
 			}
 			
-		}else if(input1.charAt((input1.length())-1)=='i'&&(input1.contains("+")==false)){
+		}else if(input1.charAt((input1.length())-1)=='i'&&(input1.contains("+")==false)){ //checks if imaginary number entered
 			System.out.println("Entered number is imaginary number");
-			 real = 0;
-			 imaginary = Integer.parseInt(input1.substring(0,input1.length()-1));
+			 real = 0;//sets real to zero
+			 imaginary = Integer.parseInt(input1.substring(0,input1.length()-1));//parses imaginary number
 			
 			
-		}else {
-			System.out.println("Entered number is real");
-			 real = Integer.parseInt(input1);
-			 imaginary = 0;
+		}else { //for real number
+			System.out.println("Entered number is real"); 
+			 real = Integer.parseInt(input1);//parses real number
+			 imaginary = 0;//sets imaginary to zero
 			
 		}
 		
 		System.out.println("Real part is "+real+" and imaginary part is "+imaginary);
 	}
-	complex(String input2){
+	complex(String input2){ //parameterised constructor used
+		/*
+		 * Operations in this constructor are similar to the ones written above
+		 * If there are any doubts, refer to code written above
+		 */
 		
 		if(input2.charAt((input2.length())-1)=='i'&&(input2.contains("+")||(input2.contains("-")))) {
 			System.out.println("Entered number is complex number");
@@ -91,29 +95,29 @@ class complex{
 		System.out.println("Real part is "+real+" and imaginary part is "+imaginary);
 		
 	}
-	void addition(complex b) {
-		real = real+b.real;
-		imaginary = imaginary+b.imaginary;
-		if (imaginary<0) {
+	void addition(complex b) { //method for addition of numbers, passes object created by parameterized constructor
+		real = real+b.real; //performs addition of real
+		imaginary = imaginary+b.imaginary; //performs addition of imaginary
+		if (imaginary<0) { //for negative imaginary part
 			System.out.println("Sum is "+real+""+imaginary+"i");	
-		}else if(real==0){
+		}else if(real==0){ //if sum is purely imaginary
 		System.out.println("Sum is "+imaginary+"i");
-		}else if(imaginary==0){
+		}else if(imaginary==0){ //if sum is purely real
 			System.out.println("Sum is "+real+"-"+imaginary+"i");			
-		}else {
+		}else { //if sum is complex and imaginary part is positive
 			System.out.println("Sum is "+real+"+"+imaginary+"i");
 		}
 	}
-	void subtraction(complex b) {
-		real = real-b.real;
-		imaginary = imaginary-b.imaginary;
-		if (imaginary<0) {
+	void subtraction(complex b) { //for subtraction of numbers, passes object created by parameterized constructor
+		real = real-b.real; //performs subtraction of real part
+		imaginary = imaginary-b.imaginary; //performs subtraction of imaginary
+		if (imaginary<0) { //if imaginary part is negative
 			System.out.println("Sum is "+real+""+imaginary+"i");	
-		}else if(real==0){
+		}else if(real==0){ //if real part is zero and purely imaginary output
 		System.out.println("Sum is "+imaginary+"i");
-		}else if(imaginary==0){
+		}else if(imaginary==0){ //if imaginary part is zero and purely real output
 			System.out.println("Sum is "+real+"-"+imaginary+"i");			
-		}else {
+		}else {//if complex and imaginary 
 			System.out.println("Sum is "+real+"+"+imaginary+"i");
 		}
 		
