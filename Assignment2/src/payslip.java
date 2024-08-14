b89acae658f498fd0e5a740c19b6432b943ac76a
Neevs1 <neevan.redkar@gmail.com>
2024-08-14 14:23:57 +0530

    Added phone number + validation

--- Assignment2/src/payslip.java
+++ Assignment2/src/payslip.java
@@ -10,15 +10,15 @@ import java.util.*;
 class employee{ //defined class employee
 	Scanner sc = new Scanner(System.in);
 	String emp_name,address,mail_id; 
-	int mobile_no,emp_id;
+	int emp_id;
+	long mob_no;
 	//defined required variables
 	void getdata() {
-		boolean validity;
+		boolean validity,valid;
 		System.out.println("Enter employee id");
 		emp_id = sc.nextInt();
 		System.out.println("Enter name");
 		emp_name = sc.next();
-		
 		do{
 		    System.out.println("Enter email id");
 		    mail_id = sc.next();
@@ -47,6 +47,18 @@ class employee{ //defined class employee
 		 }
 		
 		}while(validity == false);
+		do{
+			
+			System.out.println("Enter phone number");
+			mob_no=sc.nextLong();			
+			if((mob_no/1000000000)>0&&(mob_no/1000000000)<10) {
+				valid=true;				
+			}else {
+				valid=false;
+				System.out.println("Invalid phone number");
+			}
+		}while(valid==false);
+		
 		
 		
 	}
