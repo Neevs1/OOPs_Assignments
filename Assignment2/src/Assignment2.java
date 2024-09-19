41948e6f9eecad1a95d7e89129ae01a8e1964d7f
Neevs1 <neevan.redkar@gmail.com>
2024-09-19 19:08:45 +0530

    Changed a sentence, added comments to assignment 2

--- Assignment2/src/Assignment2.java
+++ Assignment2/src/Assignment2.java
@@ -1,31 +1,31 @@
 import java.util.*;
 import java.time.Month;
 
-class publication{
+class publication{ //base class for program
 	Scanner sc = new Scanner(System.in);
-	String title;
+	String title; //common variable declaration
 	int copies,price;
 	double purchaseCost;
-	void saleCopy(){
+	void saleCopy(){ //common method
 		System.out.println("Number of copies of "+title+" are "+copies+" at "+price);
 	}
 	
 }
 
-class book extends publication{
-	String author;
+class book extends publication{ //declared book class
+	String author; //distinct variables
 	int order;
-	void getdata() {
+	void getdata() { //get data from publication
 		System.out.println("Enter title of book(Use underscore instead of spaces)");
 		title = sc.next();
 		System.out.println("Enter author of book(Use underscores in stead of spaces)");
 		author=sc.next();
 		
-		do {
+		do { //validation for copies
 			System.out.println("Enter number of copies available");
 			copies=sc.nextInt();
 			if(copies<1) {
-				System.out.println("Invalid amount of copies \nPlease enter price again");
+				System.out.println("Invalid amount of copies \nPlease enter copies again");
 			}
 		}while(copies<1);
 		do {
@@ -34,22 +34,22 @@ class book extends publication{
 			if(price<1) {
 				System.out.println("Invalid price \nPlease enter price again");
 			}
-		}while(price<1);
-		System.out.println("Thank you, data has been entered");
+		}while(price<1); //validation for price
+		System.out.println("Thank you, data has been entered"); //completion of data entry
 	}
-	void orderCopies() {
-		if (this.copies==0){
+	void orderCopies() { // method for ordering
+		if (this.copies==0){ //validation for out of stock
 			System.out.println("Sorry, this book is not in stock");
 		}else {
 			
-			do {
+			do { //validation for orders
 				System.out.println("Great choice! Please enter number of copies you would like to purchase");
 				order = sc.nextInt();
 				if(order<0) {
 					System.out.println("Inavlid input, please re-enter");
 				}
 				}while(order<0);
-			if(order>this.copies) {
+			if(order>this.copies) { //if copies are less than ordered
 				System.out.println("Sorry, only "+this.copies+" copies are available");
 				System.out.println("Do you wish to buy "+this.copies+" books?(Respond with Yes if yes)");
 				String cont = sc.next().toLowerCase();
@@ -60,10 +60,10 @@ class book extends publication{
 					this.purchaseCost=this.copies*this.price;
 					System.out.println("Your total cost for "+this.copies+" of "+this.title+" is "+this.purchaseCost);
 					this.copies = 0;
-				}else {
+				}else { //purchase cancellation
 					System.out.println("We are sorry for the inconvenience");
 				}
-			}else {
+			}else { //purchase completion
 				System.out.println("Thank you for the purchase");
 				this.purchaseCost=this.copies*this.price;
 				System.out.println("Your total cost for "+this.copies+" of "+this.title+" is "+this.purchaseCost);
@@ -76,13 +76,13 @@ class book extends publication{
 	
 }
 
-class magazine extends publication{
+class magazine extends publication{ //magazine class declared
 	int issue;
 	int issues[];
-	int order;
+	int order; //unique variables
 	
 	
-	void getdata() {
+	void getdata() { //method to get data for publication
 		System.out.println("Enter title of Magazine(Use underscore instead of spaces)");
 		title = sc.next();
 		System.out.println("Enter number of month of current issue");
@@ -100,7 +100,7 @@ class magazine extends publication{
 		
 		
 	}
-	void orderQty(){
+	void orderQty(){ //method to order magazines
 		System.out.println("Please enter number of month of issue");
 		int mth=sc.nextInt();
 		if(issues[mth-1]==0) {
@@ -140,13 +140,13 @@ class magazine extends publication{
 		
 	}
 	
-	void currentIssue() {
-		Month month = Month.of(issue);
+	void currentIssue() { //method to display current issue
+		Month month = Month.of(issue); //using month enum
 		System.out.println("Current issue of "+title+" is "+month+",2024");
 		
 	}
 	
-	void receiveIssue() {
+	void receiveIssue() { //to display all available issues
 		System.out.println("We have issues for the following months");
 		for(int i=1;i<issue;i++) {
 			if(issues[i-1]!=0) {
@@ -159,31 +159,31 @@ class magazine extends publication{
 	
 }
 
-public class Assignment2 {
+public class Assignment2 { //main class
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //main method
 		Scanner sc = new Scanner(System.in);
 		double totalCost=0;
-		int totalCopies = 0;
-		System.out.println("Vendor side");
-		book[] b= new book[2];
-		magazine[] m = new magazine[2];
-		for(int i=0;i<2;i++) {
-			b[i]=new book();
+		int totalCopies = 0; //variables for total sale calculation
+		System.out.println("Vendor side"); //vendor side
+		book[] b= new book[2]; //data of 2 books 
+		magazine[] m = new magazine[2]; //data of 2 magazines
+		for(int i=0;i<2;i++) { //get data of books
+			b[i]=new book(); 
 			System.out.println("Enter data of book number "+(i+1));
 			b[i].getdata();
 		}
-		for(int i=0;i<2;i++) {
+		for(int i=0;i<2;i++) { //get data of magazines
 			m[i]=new magazine();
 			System.out.println("Enter data of magazine number "+(i+1));
 			m[i].getdata();
 		}
-		System.out.println("\nEnd of Vendor side \n\nClient side starts here ");
+		System.out.println("\nEnd of Vendor side \n\nClient side starts here "); //purchase for client
 		System.out.println("\n\nWelcome to publications\nWhat would you like to purchase today?");
 		System.out.println("1.Books\n2.Magazines");
 		int choice = sc.nextInt();
-		do {
-			switch(choice) {
+		do { //do while for at least 1 purchase
+			switch(choice) { //option to buy either book or magazine
 			case 1:
 				System.out.println("Enter which book you want");
 				for(int i=0;i<2;i++) {
@@ -223,12 +223,12 @@ public class Assignment2 {
 			}
 			System.out.println("Do you wish to continue?");
 			System.out.println("1.Books\n2.Magazines\nPress any other number to exit");
-			choice = sc.nextInt();
+			choice = sc.nextInt(); //for multiple purchases/exit
 			
 		}while(choice==1||choice==2);
 		
 		System.out.println("You have purchased a total of "+totalCopies+" copies/quantity worth "+totalCost);
-		System.out.println("Thank you for shopping with us");
+		System.out.println("Thank you for shopping with us"); //final bill display
 		
 		
 	}
