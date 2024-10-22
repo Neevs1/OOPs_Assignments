a2fc6b9086122992164ecf3a11af825f72b45d37
Neevs1 <neevan.redkar@gmail.com>
2024-10-22 14:51:15 +0530

    Started assignment 8

---
+++ Assignment8/src/assignment8.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+class operations{
+	int rollNum,marks,id;
+	String name,address;
+	Scanner sc = new Scanner(System.in);
+	//declared required variables
+	void add() { //adds a new record
+		
+	}
+	
+	void display() { //displays all records
+		
+	}
+	
+	void delete() {
+		
+	}
+	
+	void update() {
+		
+	}
+	
+	void search() {
+		
+	}
+}
+
+public class assignment8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
